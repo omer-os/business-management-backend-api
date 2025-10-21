295a6a0d0af2c618faad8975b832b95f536acdf3
omer-os <omerchetin19@gmail.com>
2025-10-21 22:06:10 +0300

    ✨feat : added port and preparing to deploy to railway

--- src/app.ts
+++ src/app.ts
@@ -50,7 +50,7 @@ const app = new Elysia()
 
   .use(AllRoutes)
 
-  .listen(3000);
+  .listen(process.env.PORT??3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
